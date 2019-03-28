7956e18af1ffc35d36e67186200e2f3f865dbb68
Nicolas Figueroa <nicolasfigb@gmail.com>
2019-03-27 20:11:42 -0500

    added styled components and skills animation

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import H2Styled from '../styled/H2Styled';
+import H3Styled from '../styled/H3Styled';
+import PStyled from '../styled/PStyled';
+
 
 const Education = (props) => (
     <div className="Education">
         <div className="Education-container">
-            <h2>Education</h2>
+            <H2Styled name="Education"/>
             {
                 props.education.map((edu, index) => (
                         <div className="Education-item" key={`Edu - ${index}`}>
-                            <h3>
+                            <H3Styled>
                                 {edu.degree} {edu.institution}
                             
                                 <span> {edu.startDate} - {edu.endDate}</span>
-                            </h3>
+                            </H3Styled>
+                            <PStyled name={edu.description}/>
                         </div>
                 ))
             }
