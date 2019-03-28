1162ffbd60ab746b44a0585a092500e3c4c0f7a0
Nicolas Figueroa <nicolasfigb@gmail.com>
2019-03-27 23:44:54 -0500

    Added favicons plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
 
 module.exports = {
     entry: './src/index.js',
@@ -31,6 +33,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: './index.html'
-        })
+        }),
+        new FaviconsWebpackPlugin('./public/icon.png')
     ]
 }
